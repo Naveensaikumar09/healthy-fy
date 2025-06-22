30737e7a30594679dae306c88307b527bdccda71
Naveen sai <naveensai@Naveens-MacBook-Air.local>
2025-06-22 11:44:49 +0530

    Added .env file and other updates

--- healthyfy/src/pages/PredictPage.jsx
+++ healthyfy/src/pages/PredictPage.jsx
@@ -16,7 +16,7 @@ function PredictPage({ setResponseData }) { // Destructure props to get setRespo
         setIsLoading(true); // Set loading state  
 
         try {  
-            const response = await fetch("http://127.0.0.1:8000/predict", {  
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/predict`, {  
                 method: "POST",  
                 headers: {  
                     "Content-Type": "application/json",  
